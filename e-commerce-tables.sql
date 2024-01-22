aa248ff829fdffef76d976ea50b22783c91542f8
devsomsak <devrleang@gmail.com>
2024-01-22 12:16:22 +0700

    feat:create hh1-postgresql 4 tables

--- e-commerce-tables.sql
+++ e-commerce-tables.sql
@@ -1,1 +1,31 @@
--- Start writing code here
+--# Table user
+CREATE TABLE users (
+  user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 ) UNIQUE NOT NULL,
+  email  VARCHAR ( 255 ) UNIQUE NOT NULL,
+  password  VARCHAR ( 255 ) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP UNIQUE NOT NULL
+);
+
+--# Table products
+CREATE TABLE products (
+  product_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR ( 255 ) NOT NULL,
+  description  text  NULL,
+  price numeric(10,2) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP UNIQUE NOT NULL
+);
+
+--# Table categories
+CREATE TABLE categories(
+  category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP UNIQUE NOT NULL
+);
+
+--# Table product_categories
+CREATE TABLE product_categories(
+  product_category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  product_id INT REFERENCES products(product_id) ON DELETE CASCADE,	
+  category_id INT REFERENCES categories(category_id) ON DELETE CASCADE	
+ );
